a62ae2d4fe0bed8521d6ad81508dad83492a4cde
comp500 <comp500@users.noreply.github.com>
2020-06-22 03:06:35 +0100

    Dumb attempts at making it go up blocks smoothly, also splash potions don't affect it now

--- src/main/java/link/infra/bikeshed/Bike.java
+++ src/main/java/link/infra/bikeshed/Bike.java
@@ -6,14 +6,18 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RayTraceContext;
 import net.minecraft.world.World;
 
 import java.util.Collections;
@@ -21,9 +25,13 @@ import java.util.Collections;
 @SuppressWarnings("EntityConstructor") // Stupid mcdev
 public class Bike extends LivingEntity {
 	public float wheelPosition = 0;
+	private float leanBlockDistance = 0F;
+	private float lastLeanBlockDistance = 0F;
+	public static final Vec3d BACK_WHEEL_OFFSET = new Vec3d(0, 0.37, -0.75);
 
 	protected Bike(EntityType<? extends LivingEntity> entityType, World world) {
 		super(entityType, world);
+		this.stepHeight = 1.0F;
 	}
 
 	@Override
@@ -114,5 +122,60 @@ public class Bike extends LivingEntity {
 			Vec3d backwardsOffset = new Vec3d(0, 0, -0.45).rotateY((float) Math.toRadians(-bodyYaw));
 			passenger.updatePosition(getX() + backwardsOffset.getX(), height, getZ() + backwardsOffset.getZ());
 		}
+	}
+
+	@Override
+	public boolean isAffectedBySplashPotions() {
+		return false;
+	}
+
+	@Override
+	public boolean canHaveStatusEffect(StatusEffectInstance effect) {
+		return false;
+	}
+
+	private static double blockCheckDistance = 2d;
+
+	@Override
+	public void tickMovement() {
+		this.lastLeanBlockDistance = this.leanBlockDistance;
+
+		Vec3d backWheelPos = getPos().add(BACK_WHEEL_OFFSET.rotateY((float) Math.toRadians(-bodyYaw)));
+//		Vec3d searchPos = null;
+//		for (double i = 0.5d; i < blockCheckDistance + 0.5d; i++) {
+//			searchPos = backWheelPos.add(new Vec3d(0, 0, i).rotateY((float) Math.toRadians(-bodyYaw)));
+//			BlockPos blockInFront = new BlockPos(searchPos);
+//			BlockPos blockInFrontAbove = new BlockPos(searchPos.add(0, 1, 0));
+//			// If there is a collidable block above, stop searching
+//			if (!world.getBlockState(blockInFrontAbove).getCollisionShape(world, blockInFrontAbove).isEmpty()) {
+//				searchPos = null;
+//				break;
+//			} else if (!world.getBlockState(blockInFront).getCollisionShape(world, blockInFront).isEmpty()) {
+//				// If there is a collidable block in front, finish searching
+//				break;
+//			}
+//			searchPos = null;
+//		}
+//
+//		if (searchPos == null) {
+//			this.leanBlockDistance = 0.0F;
+//		} else {
+//			double searchDistance = backWheelPos.distanceTo(searchPos);
+//			// TODO: make it work with the edge of the block?
+//			this.leanBlockDistance = (float) searchDistance;
+//		}
+		Vec3d end = backWheelPos.add(new Vec3d(0, 0, blockCheckDistance).rotateY((float) Math.toRadians(-bodyYaw)));
+		BlockHitResult res = world.rayTrace(new RayTraceContext(backWheelPos, end, RayTraceContext.ShapeType.COLLIDER, RayTraceContext.FluidHandling.NONE, this));
+		if (res.getType() == HitResult.Type.BLOCK) {
+			leanBlockDistance = (float) backWheelPos.distanceTo(res.getPos());
+		} else {
+			leanBlockDistance = 0.0F;
+		}
+
+		super.tickMovement();
+	}
+
+	public float getLeanBlockDistance(float tickDelta) {
+		return MathHelper.lerp(tickDelta, lastLeanBlockDistance, leanBlockDistance);
 	}
 }
--- src/main/java/link/infra/bikeshed/client/BikeEntityRenderer.java
+++ src/main/java/link/infra/bikeshed/client/BikeEntityRenderer.java
@@ -3,8 +3,12 @@ package link.infra.bikeshed.client;
 import link.infra.bikeshed.Bike;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Quaternion;
+import net.minecraft.util.math.Vec3d;
 
 public class BikeEntityRenderer extends LivingEntityRenderer<Bike, BikeModel> {
 	public BikeEntityRenderer(EntityRenderDispatcher dispatcher) {
@@ -20,5 +24,37 @@ public class BikeEntityRenderer extends LivingEntityRenderer<Bike, BikeModel> {
 	protected boolean hasLabel(Bike ent) {
 		// For some reason, this logic isn't overridden in LivingEntityRenderer, only in MobEntityRenderer
 		return super.hasLabel(ent) && (ent.shouldRenderName() || ent.hasCustomName() && ent == dispatcher.targetedEntity);
+	}
+
+	@Override
+	protected void setupTransforms(Bike entity, MatrixStack matrices, float animationProgress, float bodyYaw, float tickDelta) {
+		super.setupTransforms(entity, matrices, animationProgress, bodyYaw, tickDelta);
+		float distance = entity.getLeanBlockDistance(tickDelta);
+		//if (distance > 0.0F) {
+//			// Rotate around the back wheel
+//			Vec3d rotationPointOffset = Bike.BACK_WHEEL_OFFSET.rotateY((float) Math.toRadians(-entity.bodyYaw));
+//			matrices.translate(rotationPointOffset.getX(), rotationPointOffset.getY(), rotationPointOffset.getZ());
+//			// TODO: clamp?
+//			//matrices.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(animationProgress % 360f));
+//			matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(entity.bodyYaw));
+//			matrices.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(animationProgress % 360f));
+//			matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(-entity.bodyYaw));
+//			//matrices.multiply(Vector3f.POSITIVE_X.getRadialQuaternion((float) Math.atan(1.0F / distance)));
+//			// TODO: disable to test rot point?
+//			matrices.translate(-rotationPointOffset.getX(), -rotationPointOffset.getY(), -rotationPointOffset.getZ());
+
+			matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(MathHelper.lerp(tickDelta, entity.prevBodyYaw, entity.bodyYaw)));
+			// Rotate around the back wheel
+			matrices.translate(Bike.BACK_WHEEL_OFFSET.getX(), Bike.BACK_WHEEL_OFFSET.getY(), Bike.BACK_WHEEL_OFFSET.getZ());
+			// TODO: clamp?
+			//matrices.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(animationProgress % 360f));
+
+			matrices.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(animationProgress % 360f));
+
+			//matrices.multiply(Vector3f.POSITIVE_X.getRadialQuaternion((float) Math.atan(1.0F / distance)));
+
+			matrices.translate(-Bike.BACK_WHEEL_OFFSET.getX(), -Bike.BACK_WHEEL_OFFSET.getY(), -Bike.BACK_WHEEL_OFFSET.getZ());
+			matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(-MathHelper.lerp(tickDelta, entity.prevBodyYaw, entity.bodyYaw)));
+		//}
 	}
 }
